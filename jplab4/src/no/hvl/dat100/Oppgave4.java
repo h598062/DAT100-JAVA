6ff9287c7eae3f5ac456f1488057d69e7af26b0b
Spirevipp <bjornar.helgeland@hotmail.com>
2022-09-09 18:33:09 +0200

    jplab4 oppgave4

--- jplab4/src/no/hvl/dat100/Oppgave4.java
+++ jplab4/src/no/hvl/dat100/Oppgave4.java
@@ -6,18 +6,18 @@ public class Oppgave4 {
 		System.out.println("Del 1");
 		int i = 1;
 		while (i < 1) { // alltid false
-			System.out.println("Nr 1, i = " + i);
+			System.out.println("Nr 1, i = " + i); // ingenting blir printa av denne
 		}
 
 		System.out.println("Del 2");
 		for (int j = 1; j < 8; j = j * 2) {
-			System.out.println("Nr 2, j = " + j);
+			System.out.println("Nr 2, j = " + j); // 1 2 4
 		}
 
 		System.out.println("Del 3");
 		String s = "Test";
 		for (int k = s.length() - 1; k > 0; k = k - 1) {
-			System.out.println("Nr 3, k = " + s.charAt(k));
+			System.out.println("Nr 3, k = " + s.charAt(k)); // t s e
 		}
 
 		System.out.println("Del 4");
@@ -27,8 +27,8 @@ public class Oppgave4 {
 		do {
 			hjelp = hjelp / 10;
 			aS    = aS + 1;
-		} while (hjelp > 0);
+		} while (hjelp > 0);    // Denne vil kjøre en gang for hver siffer plass i input taLL
 
-		System.out.println(tall + " <fyll inn> " + aS + " <fyll inn>");
+		System.out.println(tall + " har " + aS + " siffer");
 	}
 }
