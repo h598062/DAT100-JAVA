0cba9428be4dd079a2d7ab94947caad2f2f6adf4
Spirevipp <bjornar.helgeland@hotmail.com>
2022-10-04 17:34:10 +0200

    :sparkles: Gjort ferdig OppgaveB3  :memo: Lagt inn beskrivelse av kompliserte operasjoner

--- jplab7/src/no/hvl/dat100/OppgaveB3.java
+++ jplab7/src/no/hvl/dat100/OppgaveB3.java
@@ -14,8 +14,8 @@ public class OppgaveB3 {
 			strings[i] = scanner.nextLine();
 		}
 		for (String string : strings) {
-			System.out.println(string);
-			System.out.println(snuString(string));
+			System.out.println("String vanlig: " + string);
+			System.out.println("String snudd: " + snuString(string));
 		}
 		System.out.println("Første string alfabetisk: " + nr1Str(strings));
 		System.out.println();
@@ -23,7 +23,7 @@ public class OppgaveB3 {
 		String s = scanner.nextLine();
 		int    j = 0;
 		for (String string : strings) {
-			if (string.matches(s)) {
+			if (string.matches(s)) { // regex pattern
 				j++;
 			}
 		}
@@ -34,8 +34,11 @@ public class OppgaveB3 {
 				System.out.println(string);
 			} else if (string.length() % 2 == 1) {
 				if (string.length() == 3) {
+					// hvis den kun er 3 lang så skal alle printes uansett
 					System.out.println(string);
 				} else {
+					// eks: 9 / 2 = 4, 4 er midterste tall siden vi begynner fra 0
+					// 4-1 og 4+1 blir bokstavene ved sidenav
 					System.out.println(string.charAt(string.length() / 2 - 1) + " " +
 					                   string.charAt(string.length() / 2) + " " +
 					                   string.charAt(string.length() / 2 + 1));
@@ -44,6 +47,8 @@ public class OppgaveB3 {
 				if (string.length() == 2) {
 					System.out.println(string);
 				} else {
+					// eks: 10 / 2 = 5, 5 blir teknisk sett bokstav nr 6 siden begynner på 0
+					// ved partall skal de to i midten printes, altså for 10 blir det bokstav nr 5 og 6
 					System.out.println(string.charAt(string.length() / 2 - 1) + " " +
 					                   string.charAt(string.length() / 2));
 				}
@@ -55,28 +60,35 @@ public class OppgaveB3 {
 
 	public static String snuString(String s) {
 		StringBuilder newS = new StringBuilder();
-		for (int i = s.length() -1; i >= 0; i--) {
+		// start for løkke ved siste posisjon i string og gå nedover mot 0
+		for (int i = s.length() - 1; i >= 0; i--) {
+			// legg til hver char i string inn i stringbuilder
 			newS.append(s.charAt(i));
 		}
 		return newS.toString();
 	}
 
 	public static String nr1Str(String[] strings) {
-		int i = 0;
+		int    i   = 0;
 		String out = "";
 		do {
 			int j;
-			if (i == 0) {
-				j = strings[i].compareTo(strings[i+1]);
-				if (j < 0) {
-					out = strings[i+1];
+			if (i == 0) { // unik håndtering "første" runde
+				// lagre svaret i j variabel
+				j = strings[i].compareTo(strings[i + 1]);
+				// hvis j er mer enn 0 så er strings[i] "større" enn strings[i+1]
+				// dette er da større i som at den kommer etter alfabetisk
+				if (j > 0) {
+					// lagre den første av stringsene i alfabetiske rekkefølge i variabel out
+					out = strings[i + 1];
 				} else {
 					out = strings[i];
 				}
 				i++;
 			} else {
+				// samme som i if statement, bare at vi sammenligner med den vi fant forrige loop
 				j = out.compareTo(strings[i]);
-				if (j < 0) {
+				if (j > 0) {
 					out = strings[i];
 				}
 			}
