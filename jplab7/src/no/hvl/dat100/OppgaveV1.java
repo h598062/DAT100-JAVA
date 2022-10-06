0d3f4a13a574c078aa4445410d88281ea124d406
Spirevipp <bjornar.helgeland@hotmail.com>
2022-10-06 23:33:26 +0200

    :sparkles: Gjort OppgaveV1

---
+++ jplab7/src/no/hvl/dat100/OppgaveV1.java
@@ -0,0 +1,15 @@
+package no.hvl.dat100;
+
+import java.util.Arrays;
+
+public class OppgaveV1 {
+	public static void main(String[] args) {
+		int[][] matrix = {{411, 894, 785, 643, 640},
+		                  {81, 855, 457, 265, 410},
+		                  {625, 672, 519, 547, 208},
+		                  {354, 350, 169, 449, 782},
+		                  {460, 247, 361, 181, 420}};
+		// hehe, juks
+		System.out.println(Arrays.deepToString(matrix));
+	}
+}
